6bd4124dc88467701aed3021d093b49283a053e6
Vishal <neharkar.vishal@gmail.com>
2019-11-14 23:38:00 +0530

    Init...🚀

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-console": "off",
+    "global-require": "off",
+    "no-unused-vars": "off",
+    "consistent-return": "off",
+    "no-param-reassign": "off",
+    "import/no-dynamic-require": "off",
+    "linebreak-style": ["error", "windows"],
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 1 }],
+    "max-len": ["error", 80, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: false,
+      ignoreTemplateLiterals: false,
+    }],
+  },
+};
